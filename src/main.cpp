72fce681b7d5746e52801cbbeb151709062f29c5
Puzzl3d3d <olivernorval08@outlook.com>
2023-08-20 20:40:14 +0100

    defragmentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
+// Built-ins
 #include <iostream>
 #include <string>
 #include <vector>
 #include <sstream>
 #include <cstring>
 
+// External libraries
 #include <curl/curl.h>
 #include "sha1/sha1.h"
 
@@ -11,7 +13,7 @@ using namespace std;
 
 string url = "https://api.pwnedpasswords.com/range/";
 
-size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string* data) {
+size_t writeFunction(void *ptr, size_t size, size_t nmemb, string* data) {
     data->append((char*) ptr, size * nmemb);
     return size * nmemb;
 }
@@ -30,12 +32,39 @@ vector<string> splitString(const string& str, const char _char)
     return tokens;
 }
 
+string request(string url, string hash) {
+    string response;
+    auto curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+    curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
+    curl_easy_setopt(curl, CURLOPT_FORBID_REUSE, 1L); 
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writeFunction);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L); // Disable debug printing
+
+    curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+
+    char* effective_url;
+    long response_code;
+    double elapsed;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+    curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &elapsed);
+    curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &effective_url);
+
+    return response;
+}
+
+// Constants
 int HASH_SIZE = 40;
 
 int main() {
     cout << "Enter password: ";
     string line;
-    getline(cin, line);
+    getline(cin, line);;
 
     const char* str = line.c_str();
 
@@ -61,34 +90,7 @@ int main() {
     string subbedHash = hash.substr(0,5);
     string hashLeft = hash.substr(5,hash.size() - 5);
 
-    string fullURL = url + subbedHash;
-
-    string response;
-
-    // cURL
-
-    auto curl = curl_easy_init();
-
-    curl_easy_setopt(curl, CURLOPT_URL, fullURL.c_str());
-    curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
-    curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
-    curl_easy_setopt(curl, CURLOPT_FORBID_REUSE, 1L); 
-
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writeFunction);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L); // Disable debug printing
-
-    curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
-
-    char* url;
-    long response_code;
-    double elapsed;
-    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-    curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &elapsed);
-    curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &url);
-
-    // end cURL
+    string response = request(url + subbedHash, subbedHash);
 
     vector<string> hashes = splitString(response, '\n');
 
